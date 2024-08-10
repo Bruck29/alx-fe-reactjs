8f4efb09a9cdb02e5ccd7600e99034fd71602ba9
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-08-10 21:48:50 +0300

    3rd

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
-
-
 function WelcomeMessage() {
     return (
         <div>
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!</p>
+            <WelcomeMessage />
         </div>
     );
 }
