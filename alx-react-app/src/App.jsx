6d3f6325afa22454008f2bd9133620f51e608ebf
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-08-11 13:53:24 +0300

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
