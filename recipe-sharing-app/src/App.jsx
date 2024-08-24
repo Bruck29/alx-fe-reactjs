9cacbc2f8ccc2c3bc6e9fb1c364327cbf491a2ad
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-08-24 15:03:34 +0300

    update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,12 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
     <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
