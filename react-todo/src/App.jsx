3c6a1fb448defec178f6e5e69e198c1b6317ff91
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-09-01 16:11:15 +0300

    update app.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoList />
+    </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
