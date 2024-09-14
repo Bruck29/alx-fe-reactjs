d336731e22a85d6ff49fae238f6cb4ebaf93a99d
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-09-14 14:18:02 +0300

    update tailwind

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, 
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
