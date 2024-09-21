b091ab116195592c3ef8652ad5a1ff5190fb423b
Nebiyou Belaineh <Bruckasfaw0@gmail.com>
2024-09-21 16:12:03 +0300

    Update git.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const fetchAdvancedUserData = async (username, location, minRepos) => {
+export const fetchAdvancedUserData = async (username, location, minRepos, page = 1) => {
   let query = `q=${username ? `${username}+` : ''}`;
   if (location) query += `location:${location}+`;
   if (minRepos) query += `repos:>=${minRepos}`;
 
-  const response = await axios.get(`${BASE_URL}/search/users?${query}`);
+
+  const response = await axios.get(`${BASE_URL}/search/users?${query}&page=${page}`);
   return response.data;
 };
